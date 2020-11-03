4087102f53c4ebf3305fb7e65c04d6d9c1a5f80e
Dan Dart <git@dandart.co.uk>
2020-11-03 14:35:06 +0000

    all the updates

--- code/updates.sh
+++ code/updates.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 INITDIR=~/code
 cd $INITDIR
 # Clear deps
@@ -15,6 +16,24 @@ do
         echo Updating $BASE...
         cd $DIR
         stack build
+        echo Finished updating $BASE
+        cd $INITDIR
+    done
+done
+echo Finding JS projects...
+for DIRLOC in ~/code/mine/js ~/code/mine/multi/projects/js
+do
+    echo Using repos location $DIRLOC
+    find -name package-lock.json -delete
+    find -name yarn.lock -delete
+    for STACKS in $(find $DIRLOC -path "*node_modules*" -prune -o -name package.json)
+    do
+        DIR=$(dirname $STACKS)
+        BASE=$(basename $DIR)
+        echo Updating $BASE...
+        cd $DIR
+        ncu -un
+        npm install || echo "Nah"
         echo Finished updating $BASE
         cd $INITDIR
     done
